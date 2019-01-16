1f9d9795f4e7742240a38fcefa29211b91a3f6ea
Lizzie Chan <lchan217@gmail.com>
2019-01-16 16:42:28 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,7 +34,7 @@ def turn(board)
   puts "Please enter 1-9:"
   answer = gets.strip
   response_index = input_to_index(answer)
-  if valid_move(board,response_index) 
+  if valid_move(board,response_index) == true
     display_board(board=[])
   else 
     puts "This is not valid"
