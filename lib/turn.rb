e3a93032be83bcc61b58ebf8989d10f2dca34083
Lizzie Chan <lchan217@gmail.com>
2019-01-16 16:36:41 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,8 +6,8 @@ def display_board(board=[])
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
   
-def valid_move?(board,response_index)
-response_index.between?(0,8) && !position_taken?(board,response_index)
+def valid_move?(board,index)
+index.between?(0,8) && !position_taken?(board,index)
 end
   
 def move(board,position,value="X")
