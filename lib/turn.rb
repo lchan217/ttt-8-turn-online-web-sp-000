f677144aca66932e0ccae2a570ced3f2a6042bb4
Lizzie Chan <lchan217@gmail.com>
2019-01-16 16:43:16 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,7 +35,7 @@ def turn(board)
   answer = gets.strip
   response_index = input_to_index(answer)
   if valid_move(board,response_index) == true
-    move(board,response_index,value="X")
+    move(board,response_index)
     display_board(board=[])
   else 
     puts "This is not valid"
