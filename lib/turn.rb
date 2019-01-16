81905921f9c52da1f79ea54266f7c208926f043b
Lizzie Chan <lchan217@gmail.com>
2019-01-16 16:31:00 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,44 @@
+def display_board(board=[])
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+  
+def valid_move?(board,index)
+index.between?(0,8) && !position_taken?(board,index)
+end
+  
+def move(board,position,value="X")
+board[position] = value
+end
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    true
+  elsif board[index] == " "  
+    false
+  elsif board[index] == ""
+    false
+  else  
+    false
+  end
+end
+
+def input_to_index(user_input)
+  user_input.to_i-1
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  answer = gets.strip
+  response_index = input_to_index(answer)
+  
+  counter = 0
+  until valid_move?(board,response_index) == true
+    puts "This is not valid"
+    counter+=1
+    turn(board)
+  end
+end
